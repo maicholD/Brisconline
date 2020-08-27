7ff9e9ee8d985206dda09284ebe1ec266a49780a
Lorenzo <lorenzo.simoncini2@studio.unibo.it>
2020-08-27 17:32:03 +0200

    formatted code

--- public/js/404.js
+++ public/js/404.js
@@ -1,7 +1,7 @@
 const NotFound = {
    template: `
    <div id="notFound-body">
-     <h1 >404 Pagina non trovata, se sei triste per non averla trovata, goditi il video</h1>
+     <h1 >404 Pagina non trovata, se sei triste per non averla trovata goditi il video</h1>
      <iframe
       src="https://www.youtube.com/embed/AUYgAt8DJm4?autoplay=1&mute=1">
      </iframe>
